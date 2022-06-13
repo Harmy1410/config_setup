3a6f6090db67169332ed45d99a383d7993bf6014
Harmy1410 <harmy1410@gmail.com>
2022-06-13 14:20:31 -0400

    hello world reset

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,3 @@
-#[derive(Debug)]
-struct PathToConfig {
-    path: String,
-}
-
-const DEV_PATH: &str = "/Desktop";
-// const PROD_PATH: &str = "/Developer";
-
 fn main() {
-    let mmc = PathToConfig::new();
-    println!("{:#?}", mmc.path);
-    if let Some(path) = home::home_dir() {
-        println!("{}", path.display());
-    }
-}
-
-impl PathToConfig {
-    fn new() -> PathToConfig {
-        let show_sarcasm: bool = std::env::args().skip(1).len() > 1;
-        if show_sarcasm {
-            println!("no need to pass unnecessary stuff!!!\n");
-        }
-
-        let default_path_to_config = String::from(DEV_PATH);
-        // let default_path_to_config = String::from(prod_path);
-        let provided_path = std::env::args().nth(1);
-        if let Some(path) = provided_path {
-            PathToConfig { path }
-        } else {
-            println!("Path not provided! Checking default path: {default_path_to_config}\n");
-            PathToConfig {
-                path: default_path_to_config,
-            }
-        }
-    }
+    println!("hello world")
 }
