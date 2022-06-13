bb8a83a0377fbad7b3a248f15f6c448e7d0cac31
Harmy1410 <harmy1410@gmail.com>
2022-06-13 18:35:47 -0400

    reading json

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("hello world")
+use serde_json::{Result, Value};
+use std::io::Read;
+
+fn main() -> std::io::Result<()> {
+    let mut json = std::fs::File::open("/Users/harmeepatel/Developer/projects/rust/projects/config_setup/personal_config_symlinks.json")?;
+    let mut buf = String::new();
+
+    json.read_to_string(&mut buf)?;
+    println!("{buf}");
+    untyped_example(&buf)?;
+
+    Ok(())
+}
+
+fn untyped_example(data: &String) -> Result<()> {
+    // Parse the string of data into serde_json::Value.
+    let v: Value = serde_json::from_str(&data.to_string())?;
+
+    // Access parts of the data by indexing with square brackets.
+    println!("{}", v["one"]);
+
+    Ok(())
 }
