d30a23d223f069267008f33013b7e1086d1fd798
Harmy1410 <harmy1410@gmail.com>
2022-06-13 14:26:29 -0400

    reverting hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+#[derive(Debug)]
+struct PathToConfig {
+    path: String,
+}
+
+const DEV_PATH: &str = "/Desktop";
+// const PROD_PATH: &str = "/Developer";
+
 fn main() {
-    println!("hello world")
+    let mut mmc = PathToConfig::new();
+    println!("{:#?}", mmc.path);
+    if let Some(mut path) = home::home_dir() {
+        println!("{:?}", path.push(&mmc.path));
+    }
+}
+
+impl PathToConfig {
+    fn new() -> PathToConfig {
+        let show_sarcasm: bool = std::env::args().skip(1).len() > 1;
+        if show_sarcasm {
+            println!("no need to pass unnecessary stuff!!!\n");
+        }
+
+        let default_path_to_config = String::from(DEV_PATH);
+        // let default_path_to_config = String::from(prod_path);
+        let provided_path = std::env::args().nth(1);
+        if let Some(path) = provided_path {
+            PathToConfig { path }
+        } else {
+            println!("Path not provided! Checking default path: {default_path_to_config}\n");
+            PathToConfig {
+                path: default_path_to_config,
+            }
+        }
+    }
 }
