cce3b6bdb10d1725d4646d7fc468b01c5d4c98fb
Harmy1410 <harmy1410@gmail.com>
2022-06-13 21:05:37 -0400

    cleaning up.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
-use serde_json::{Result, Value};
+use serde_json::Value;
 use std::io::Read;
 
-fn main() -> std::io::Result<()> {
-    let mut json = std::fs::File::open("/Users/harmeepatel/Developer/projects/rust/projects/config_setup/personal_config_symlinks.json")?;
+fn main() {
+    let path = "/Users/harmeepatel/Developer/projects/rust/projects/config_setup/personal_config_symlinks.json";
     let mut buf = String::new();
+    let mut json_file = match std::fs::File::open(path) {
+        Ok(file) => file,
+        Err(err) => panic!("{}", err),
+    };
 
-    json.read_to_string(&mut buf)?;
-    println!("{buf}");
-    println!("{}", std::any::type_name<&str>());
-    untyped_example(&buf)?;
-
-    Ok(())
+    match json_file.read_to_string(&mut buf) {
+        Ok(_) => parse_json(&buf.to_string()),
+        Err(err) => panic!("{}", err),
+    }
 }
 
-fn untyped_example(data: &String) -> Result<()> {
-    let v: Value = serde_json::from_str(&data.to_string())?;
-
-    println!("{}", v["one"]);
-
-    Ok(())
+fn parse_json(data: &str) {
+    let data: Value = serde_json::from_str(data).unwrap();
+    println!("{}", data["one"]);
+    println!("{}", data["two"]);
+    println!("{}", data["three"]);
 }
