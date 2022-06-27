2c6860083a23ad8f18814c1554504e988f143c14
Harmee Patel <harmeepatel@Harmees-MBP.hitronhub.home>
2022-06-27 01:03:20 -0400

    only creating if necessary

--- src/symlink_arr.rs
+++ src/symlink_arr.rs
@@ -11,12 +11,26 @@ type Symlinks<'a> = Vec<Symlink<'a>>;
 
 pub fn create_syms(buf: &String) -> std::io::Result<()> {
     let arr: Symlinks = serde_json::from_str(buf)?;
+    let mut sym_link_count = 0;
 
     for i in arr.iter() {
-        if let Ok(metadata) = std::fs::symlink_metadata(i.to) {
-            dbg!(metadata.file_type().is_symlink());
-        };
-        // let hello = std::os::unix::fs::symlink(&i.from, &i.to);
+        let metadata: Result<std::fs::Metadata, &'static str> =
+            match std::fs::symlink_metadata(i.to) {
+                Ok(metadata) => Ok(metadata),
+                Err(_) => Err("Path not correct"),
+            };
+        if let Ok(metadata) = metadata {
+            if metadata.file_type().is_symlink() {
+                sym_link_count += 1;
+            }
+        } else {
+            println!("Creating link from: \t{}\n\t\tto: \t{}", i.from, i.to);
+            // std::os::unix::fs::symlink(i.from, i.to)?;
+        }
+    }
+
+    if sym_link_count == arr.len() {
+        println!("Everything is fine!!");
     }
 
     Ok(())
