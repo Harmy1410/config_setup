ae9e25d0dd6f89d0177412133d5869fe4ca2ef57
Harmee Patel <harmeepatel@Harmees-MBP.hitronhub.home>
2022-08-03 23:54:34 -0400

    using clap for args

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,43 @@
 use std::io::Read;
-mod path_to_config;
 mod symlink_arr;
+use clap::Parser;
+
+/// Program to setup all config files with a json file.
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Path to config file
+    #[clap(
+        short,
+        long,
+        value_parser,
+        default_value = "/Developer/mmc/config_sym.json"
+    )]
+    path: String,
+}
 
 fn main() -> std::io::Result<()> {
-    // file for this
-    let config = path_to_config::PathToConfig::new();
+    let mut args = Args::parse();
+
+    let rel_path = args.path.to_owned();
+
+    if let Ok(mut home) = std::env::var("HOME") {
+        home.push_str(&args.path);
+        args.path = home;
+    } else {
+        println!("Try setting $HOME variable.");
+    }
+
+    println!(
+        "Path not provided! Checking default path: $HOME {}\n",
+        &args.path
+    );
 
     let mut buf = String::new();
-    std::fs::File::open(&config.path)?.read_to_string(&mut buf)?;
+    std::fs::File::open(&args.path)?.read_to_string(&mut buf)?;
 
     // file for this
-    symlink_arr::create_syms(&buf, &config.path);
+    symlink_arr::create_syms(&buf, &args.path);
 
     Ok(())
 }
