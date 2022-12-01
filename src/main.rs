17ba6409d15819d84291c755516da3a8ff741452
Harmy1410 <harmy1410@gmail.com>
2022-12-01 00:19:51 -0500

    idk🤷

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(name = "setup_config", author = "harmeepatel", version = "2.0.0", about = "CLI tool to put files in place.", long_about = None)]
+struct CLI {
+    /// Name of the person to greet
+    #[arg(short = 'p', long, default_value_t = String::from("~/Developer/mmc/config_sym.json"))]
+    path: String,
+
+    /// Number of times to greet
+    #[arg(short = 'w', long, default_value_t = 0)]
+    write: u8,
+}
+
 fn main() {
-    println!("hello world")
+    let args = CLI::parse();
+
+    println!("Hello {}!", &args.path)
 }
